f7c0fbe8bd7fb0b5f258ab15062e297bdac0233c
Harish1651999 <hk1651999@gmail.com>
2024-03-04 08:46:12 +0530

    add meals-grid and meal-item component.

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,23 +1,27 @@
-import Link from "next/link";
 import React from "react";
+import classes from "./page.module.css";
+import Link from "next/link";
+import MealsGrid from "@/components/meals/meals-grid";
 
 const MealsPage = () => {
   return (
-    <main>
-      <h1 style={{ color: "white", textAlign: "center" }}>Meals Page</h1>
-      <Link
-        style={{ color: "white", textAlign: "center", display: "block" }}
-        href="/meals/post-1"
-      >
-        Meals 1
-      </Link>
-      <Link
-        style={{ color: "white", textAlign: "center", display: "block" }}
-        href="/meals/post-2"
-      >
-        Meals 2
-      </Link>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favourite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favourite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 };
 
